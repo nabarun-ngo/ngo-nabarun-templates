3e4b1cb0f0a13228f275c7b32e863f1d471761bd
souvik-sarkar-98 <59085419+souvik-sarkar-98@users.noreply.github.com>
2025-07-27 15:07:14 +0530

    Create run_cucumber_tests.sh

---
+++ scripts/run_cucumber_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Usage:
+# ./run_cucumber_tests.sh "<scenario_string>" <job_index> <env> <doppler_project_name> <doppler_token> <max_reruns>
+
+set -e
+
+SCENARIO_STRING="$1"
+JOB_INDEX="$2"
+TEST_ENV="$3"
+DOPPLER_PROJECT_NAME="$4"
+DOPPLER_SERVICE_TOKEN="$5"
+MAX_RETRIES="$6"
+
+ATTEMPT=0
+TEST_FAILED=0
+cd test
+echo "🏁 Starting test run for job index: $JOB_INDEX"
+echo "🔢 Max Rerun Attempts: $MAX_RETRIES"
+echo "📋 Scenarios: $SCENARIO_STRING"
+
+mvn clean test -q \
+      -Dcucumber.features="$SCENARIO_STRING" \
+      -Dsurefire.rerunFailingTestsCount=$MAX_RETRIES \
+      -Dcucumber.plugin="pretty,html:target/cucumber-${JOB_INDEX}.html,json:target/cucumber-${JOB_INDEX}.json,junit:target/cucumber-${JOB_INDEX}.xml" \
+      -DENVIRONMENT=$TEST_ENV \
+      -DCONFIG_SOURCE=doppler \
+      -DDOPPLER_PROJECT_NAME=$DOPPLER_PROJECT_NAME \
+      -DDOPPLER_SERVICE_TOKEN=$DOPPLER_SERVICE_TOKEN || TEST_FAILED=1
+
+if [ $TEST_FAILED -eq 1 ]; then
+  echo "TEST_STATUS=FAILED" >> $GITHUB_ENV
+else
+  echo "TEST_STATUS=SUCCESS" >> $GITHUB_ENV
+fi
